820e9d6c9bae3ab63652142ed1550cfef33a242e
Gaurav Shankar Kumar <gaurav@Gauravs-MacBook-Pro.local>
2021-11-30 20:42:40 +0530

    heroku

--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -123,10 +123,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = "/var/www/pesronal-portfolio.herokuapp.com/static/"
 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
+    'var/www/static/',
 )
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
