8d6ffb6f23576d4360c2ddbaadca5c8c7365abb3
Gaurav Shankar Kumar <gaurav@Gauravs-MacBook-Pro.local>
2021-11-30 20:53:08 +0530

    heroku

--- personal_portfolio/urls.py
+++ personal_portfolio/urls.py
@@ -27,6 +27,6 @@ urlpatterns = [
     path('blog/', include('blog.urls')),
     re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
 
-]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+]+ static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS[0])
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
