c94246e4d994689de0f07cb3a7e54a658174af80
Gaurav Shankar Kumar <gaurav@Gauravs-MacBook-Pro.local>
2021-11-30 17:08:26 +0530

    setup

--- personal_portfolio/urls.py
+++ personal_portfolio/urls.py
@@ -14,10 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.conf.urls.static import static
 from django.conf import settings
 from portfolio import views
+from django.views.static import serve
 
 
 urlpatterns = [
@@ -26,6 +27,6 @@ urlpatterns = [
     path('blog/', include('blog.urls')),
     re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
 ]
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS[0])
+# if settings.DEBUG:
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS[0])
