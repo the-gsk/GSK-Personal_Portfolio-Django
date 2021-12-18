a57c888198958c53c8401a60a2f185745c2867f8
gaurav kumar <gauravshankarkumar.com>
2021-12-18 12:38:37 +0530

    maping url

--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-k4)c_fuusn07&^=rjlytdu(77&^)1v-oq0&1npoy_ca^$_xs7r
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['pesronal-portfolio.herokuapp.com','127.0.0.1']
+ALLOWED_HOSTS = ['gsk-pesronalportfolio.herokuapp.com','127.0.0.1']
 
 
 # Application definition
