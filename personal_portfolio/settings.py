bcee2f300f3a12c6d0163c8f4d8b61d990b4338b
Gaurav Shankar Kumar <gauravshankarkumar@gmail.com>
2022-03-22 16:04:20 +0530

    Update settings.py

--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -120,8 +120,7 @@ USE_TZ = True
 # SMTP Setup 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_USE_TLS = False
-EMAIL_USE_SSL = False
+EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'heavycoder.in@gmail.com'
 EMAIL_HOST_PASSWORD = os.getenv('Email_Password')
