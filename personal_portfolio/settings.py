2cc3f53feb9651085e6083e4ce2ee94f31962735
kailas-mi <gauravshankarkumar@gmail.com>
2023-05-01 11:23:00 +0530

    updated ai

--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -153,4 +153,4 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
